7a6dab0aba4f6e1d103b2cc2d00307efddbc9db2
Elber Carneiro <elber@yieldmo.com>
2016-05-23 13:22:43 -0400

    Initial Commit

---
+++ SDKTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDKTest
+//
+//  Created by Elber Carneiro on 5/23/16.
+//  Copyright © 2016 Elber Carneiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDKTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDKTest
+//
+//  Created by Elber Carneiro on 5/23/16.
+//  Copyright © 2016 Elber Carneiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
